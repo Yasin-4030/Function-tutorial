8344a5636724ed89e6622714685272a85d9d6384
Yasin-4030 <69739329+Yasin-4030@users.noreply.github.com>
2020-09-08 20:38:59 -0700

    fun-tutorial
    CS 1.0--function.py

---
+++ functions.py
@@ -0,0 +1,44 @@
+#Hello,
+#functions........................................
+def area(width, height):
+    result = width * height
+    return result
+    
+result = area(5, 6)
+#print(result)
+
+result = area(7, 10) 
+print(result)
+
+result = area(7, 7)
+print(result)
+
+# function subtraction..............................
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+result = subtract(8, 4)
+print(result)
+
+result = subtract(90, 45)
+print(result)
+
+#function division...................................
+def divide(x, y):
+    result = x / y
+    return result
+
+result = divide(16, 4)
+print (result)
+
+#function addition...................................
+def add(a, b):
+    result = a + b
+    return result
+
+result = add( 12, 19)
+print (result)
+
+result = add( 2, 44)
+print (result)
